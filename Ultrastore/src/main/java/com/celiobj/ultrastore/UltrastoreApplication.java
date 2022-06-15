6ca839db7642c99cf997a72c62908a996f49be01
Celio Braganca Jr <celio.junior@gft.com>
2022-06-15 10:37:51 -0300

    First commit

---
+++ Ultrastore/src/main/java/com/celiobj/ultrastore/UltrastoreApplication.java
@@ -0,0 +1,13 @@
+package com.celiobj.ultrastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltrastoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltrastoreApplication.class, args);
+	}
+
+}
---
+++ Ultrastore/src/test/java/com/celiobj/ultrastore/UltrastoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celiobj.ultrastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltrastoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
